9319cec1380fceb30781a17557c81262e3ab363e
kaii <imkaiilb@gmail.com>
2025-01-18 13:15:25 +0200

    switch to apksigner

--- build.sh
+++ build.sh
@@ -21,5 +21,5 @@ fi
 JAVA_HOME=/opt/android-studio/jbr/ ./gradlew $gradleTarget ${@:2}
 
 echo "Signing...."
-LD_LIBRARY_PATH=./signapk/ java -jar signapk/signapk.jar keys/releasekey.x509.pem keys/releasekey.pk8 ./app/build/outputs/$target/${file}.apk photos_signed_$tag.apk
+./apksigner/apksigner sign --in ./app/build/outputs/$target/${file}.apk --out photos_signed_$tag.apk --key keys/releasekey.pk8 --cert keys/releasekey.x509.pem 
 echo "Signed!"
