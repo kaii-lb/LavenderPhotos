kaii-lb/LavenderPhotos

commit e0f9b4495bed4106a8fbade81a9d357e8770eced

improvement: move more string literals to resources
Signed-off-by: kaii_lb <imkaiilb@gmail.com>

app/src/main/java/com/kaii/photos/compose/app_bars/MainBars.kt
@@ -53,6 +53,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalResources
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -267,6 +268,7 @@ fun MainAppBottomBar(
                     } else {
                         val windowInfo = LocalWindowInfo.current
                         val localDensity = LocalDensity.current
+                        val resources = LocalResources.current
 
                         LazyRow(
                             state = state,
@@ -278,7 +280,13 @@ fun MainAppBottomBar(
                             items(
                                 count = tabs.size
                             ) { index ->
-                                val tab = tabs[index]
+                                val tab = tabs[index].let {
+                                    it.copy(
+                                        name =
+                                            if (it in DefaultTabs.extendedList) resources.getString(it.id)
+                                            else it.name
+                                    )
+                                }
 
                                 ToggleButton(
                                     checked = currentView.value == tab,

app/src/main/java/com/kaii/photos/datastore/PreferencesOptions.kt
@@ -22,42 +22,42 @@ object DefaultTabs {
             name = "Photos",
             albumPaths = listOf("main_photos"),
             icon = StoredDrawable.PhotoGrid,
-            id = 0
+            id = R.string.navigation_photos
         )
 
         val secure = BottomBarTab(
             name = "Secure",
             albumPaths = listOf("secure_folder"),
             icon = StoredDrawable.SecureFolder,
-            id = 1
+            id = R.string.navigation_secure
         )
 
         val albums = BottomBarTab(
             name = "Albums",
             albumPaths = listOf("albums_page"),
             icon = StoredDrawable.Albums,
-            id = 2
+            id = R.string.navigation_albums
         )
 
         val search = BottomBarTab(
             name = "Search",
             albumPaths = listOf("search_page"),
             icon = StoredDrawable.Search,
-            id = 3
+            id = R.string.navigation_search
         )
 
         val favourites = BottomBarTab(
             name = "Favourites",
             albumPaths = listOf("favourites_page"),
             icon = StoredDrawable.Favourite,
-            id = 4
+            id = R.string.navigation_favourites
         )
 
         val trash = BottomBarTab(
             name = "Trash",
             albumPaths = listOf("trash_page"),
             icon = StoredDrawable.Trash,
-            id = 5
+            id = R.string.navigation_trash
         )
     }
 

app/src/main/java/com/kaii/photos/compose/PermissionHandler.kt
@@ -52,7 +52,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.platform.LocalResources
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
@@ -122,7 +124,7 @@ fun PermissionHandler(
 
             if (showExplanationDialog.value) {
                 ExplanationDialog(
-                    title = "Permission Explanation",
+                    title = stringResource(id = R.string.permissions_explanation),
                     explanation = whyButtonExplanation,
                     showPreviousDialog = showPermDeniedDialog
                 ) {
@@ -137,7 +139,7 @@ fun PermissionHandler(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
-                    text = "Permissions",
+                    text = stringResource(id = R.string.permissions),
                     fontSize = TextUnit(22f, TextUnitType.Sp),
                     fontWeight = FontWeight.Bold,
                     color = MaterialTheme.colorScheme.onBackground
@@ -157,7 +159,7 @@ fun PermissionHandler(
                 ) {
                     item {
                         PreferencesSeparatorText(
-                            text = "Permissions"
+                            text = stringResource(id = R.string.permissions)
                         )
                     }
 
@@ -188,15 +190,16 @@ fun PermissionHandler(
                                 showPermDeniedDialog.value = !granted
                             }
 
+                            val resources = LocalResources.current
                             PermissionButton(
-                                name = "Read Images",
-                                description = "Allow Lavender Photos to discover photos on the device",
+                                name = stringResource(id = R.string.permissions_read_images),
+                                description = stringResource(id = R.string.permissions_read_images_desc),
                                 position = RowPosition.Top,
                                 granted = !mainViewModel.permissionQueue.contains(Manifest.permission.READ_MEDIA_IMAGES)
                             ) {
                                 readMediaImageLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
 
-                                whyButtonExplanation = Explanations.READ_MEDIA
+                                whyButtonExplanation = resources.getString(Explanations.READ_MEDIA)
 
                                 onGrantPermissionClicked = {
                                     val intent = Intent(
@@ -235,15 +238,16 @@ fun PermissionHandler(
                                 showPermDeniedDialog.value = !granted
                             }
 
+                            val resources = LocalResources.current
                             PermissionButton(
-                                name = "Read Videos",
-                                description = "Allow Lavender Photos to discover videos on the device",
+                                name = stringResource(id = R.string.permissions_read_videos),
+                                description = stringResource(id = R.string.permissions_read_videos_desc),
                                 position = RowPosition.Middle,
                                 granted = !mainViewModel.permissionQueue.contains(Manifest.permission.READ_MEDIA_VIDEO)
                             ) {
                                 readMediaVideoLauncher.launch(Manifest.permission.READ_MEDIA_VIDEO)
 
-                                whyButtonExplanation = Explanations.READ_MEDIA
+                                whyButtonExplanation = resources.getString(Explanations.READ_MEDIA)
 
                                 onGrantPermissionClicked = {
                                     val intent = Intent(
@@ -282,15 +286,16 @@ fun PermissionHandler(
                                 showPermDeniedDialog.value = !granted
                             }
 
+                            val resources = LocalResources.current
                             PermissionButton(
-                                name = "Read External Storage",
-                                description = "Allow Lavender Photos to discover photos and videos on the device",
+                                name = stringResource(id = R.string.permissions_read_external_storage),
+                                description = stringResource(id = R.string.permissions_read_external_storage_desc),
                                 position = RowPosition.Top,
                                 granted = !mainViewModel.permissionQueue.contains(Manifest.permission.READ_EXTERNAL_STORAGE)
                             ) {
                                 readExternalStorageLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
 
-                                whyButtonExplanation = Explanations.READ_MEDIA
+                                whyButtonExplanation = resources.getString(Explanations.READ_MEDIA)
 
                                 onGrantPermissionClicked = {
                                     val intent = Intent(
@@ -319,16 +324,17 @@ fun PermissionHandler(
                                 mainViewModel.settings.Permissions.setIsMediaManager(granted)
                             }
 
+                            val resources = LocalResources.current
                             PermissionButton(
-                                name = "Manage Media",
-                                description = "Optional permission. Is used for faster trash/delete functionality",
+                                name = stringResource(id = R.string.permissions_manage_media),
+                                description = stringResource(id = R.string.permissions_manage_media_desc),
                                 position = RowPosition.Bottom,
                                 granted = !mainViewModel.permissionQueue.contains(Manifest.permission.MANAGE_MEDIA)
                             ) {
                                 val intent = Intent(Settings.ACTION_REQUEST_MANAGE_MEDIA)
                                 manageMediaLauncher.launch(intent)
 
-                                whyButtonExplanation = Explanations.MANAGE_MEDIA
+                                whyButtonExplanation = resources.getString(Explanations.MANAGE_MEDIA)
 
                                 onGrantPermissionClicked = {
                                     manageMediaLauncher.launch(intent)
@@ -339,14 +345,14 @@ fun PermissionHandler(
 
                     item {
                         PreferencesSeparatorText(
-                            text = "Other Info"
+                            text = stringResource(id = R.string.permissions_other_info)
                         )
                     }
 
                     item {
                         PreferencesRow(
-                            title = "Install Packages",
-                            summary = "If you use the in-app updater, Lavender Photos will request the Install Packages permission.",
+                            title = stringResource(id = R.string.permissions_install_packages),
+                            summary = stringResource(id = R.string.permissions_install_packages_desc),
                             iconResID = R.drawable.error_2,
                             position = RowPosition.Single,
                             backgroundColor = MaterialTheme.colorScheme.surfaceContainer,
@@ -368,7 +374,7 @@ fun PermissionHandler(
                             modifier = Modifier
                                 .align(Alignment.CenterStart)
                         ) {
-                            Text(text = "Exit")
+                            Text(text = stringResource(id = R.string.editing_exit))
                         }
 
                         Button(
@@ -379,7 +385,7 @@ fun PermissionHandler(
                             modifier = Modifier
                                 .align(Alignment.CenterEnd)
                         ) {
-                            Text(text = "Continue")
+                            Text(text = stringResource(id = R.string.permissions_continue))
                         }
                     }
                 }
@@ -400,7 +406,7 @@ fun PermissionHandler(
                         },
                         enabled = mainViewModel.checkCanPass()
                     ) {
-                        Text(text = "Continue")
+                        Text(text = stringResource(id = R.string.permissions_continue))
                     }
 
                     Spacer(modifier = Modifier.height(8.dp))
@@ -410,7 +416,7 @@ fun PermissionHandler(
                             (context as Activity).finish()
                         }
                     ) {
-                        Text(text = "Exit")
+                        Text(text = stringResource(id = R.string.editing_exit))
                     }
                 }
             }
@@ -511,7 +517,7 @@ fun PermissionDeniedDialog(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Necessary Permission",
+                text = stringResource(id = R.string.permissions_necessary),
                 fontSize = TextUnit(18f, TextUnitType.Sp),
                 fontWeight = FontWeight.Bold,
                 color = MaterialTheme.colorScheme.onBackground,
@@ -521,7 +527,7 @@ fun PermissionDeniedDialog(
             Spacer(modifier = Modifier.height(12.dp))
 
             Text(
-                text = "This permission is necessary for app functionality",
+                text = stringResource(id = R.string.permissions_necessary_desc),
                 fontSize = TextUnit(14f, TextUnitType.Sp),
                 color = MaterialTheme.colorScheme.onBackground,
                 modifier = Modifier.wrapContentSize()
@@ -530,7 +536,7 @@ fun PermissionDeniedDialog(
             Spacer(modifier = Modifier.height(32.dp))
 
             FullWidthDialogButton(
-                text = "Grant Permission",
+                text = stringResource(id = R.string.permissions_grant_permissions),
                 color = MaterialTheme.colorScheme.primary,
                 textColor = MaterialTheme.colorScheme.onPrimary,
                 position = RowPosition.Top
@@ -540,7 +546,7 @@ fun PermissionDeniedDialog(
             }
 
             FullWidthDialogButton(
-                text = "Why?",
+                text = stringResource(id = R.string.permissions_why),
                 color = MaterialTheme.colorScheme.surfaceContainer,
                 textColor = MaterialTheme.colorScheme.onSurface,
                 position = RowPosition.Middle
@@ -549,7 +555,7 @@ fun PermissionDeniedDialog(
             }
 
             FullWidthDialogButton(
-                text = "Dismiss",
+                text = stringResource(id = R.string.permissions_dismiss),
                 color = MaterialTheme.colorScheme.surfaceContainer,
                 textColor = MaterialTheme.colorScheme.onSurface,
                 position = RowPosition.Bottom,
@@ -605,8 +611,6 @@ fun FullWidthDialogButton(
 }
 
 private object Explanations {
-    const val READ_MEDIA =
-        "This permission is needed to find photos and videos on the device. Lavender Photos is very strict with what files it reads, and never shares or exploits this info."
-    const val MANAGE_MEDIA =
-        "This permission is optional, but is highly recommended. Manage Media permission allows Lavender Photos to use Android's Content Resolver API to trash/delete/move/copy media, which makes the process much smoother and more interoperable with other apps."
+    val READ_MEDIA = R.string.permissions_read_media_explanation
+    val MANAGE_MEDIA = R.string.permissions_manage_media_explanation
 }

app/src/main/res/values-ar/strings.xml
@@ -186,7 +186,7 @@
     <string name="trash_empty">افرغ سلة المهملات؟</string>
     <string name="trash_empty_desc">يؤدي هذا إلى حذف جميع العناصر الموجودة في سلة المهملات، ولا يمكن التراجع عن هذا الإجراء</string>
     <string name="trash_empty_confirm">إفراغ</string>
-    <string name="permissions">إذن</string>
+    <string name="permission">إذن</string>
     <string name="permissions_media_manager">مدير الوسائط</string>
     <string name="permissions_media_manager_desc">إزالة/حذف/نسخ/نقل — أفضل وأسرع</string>
     <string name="permissions_media_management">إدارة الوسائط</string>

app/src/main/res/values-es/strings.xml
@@ -207,7 +207,7 @@
     <string name="trash_empty">¿Vaciar Papelera?</string>
     <string name="trash_empty_desc">Esto elimina todos los elementos de la papelera, esta acción no se puede deshacer</string>
     <string name="trash_empty_confirm">Vacío</string>
-    <string name="permissions">Permiso</string>
+    <string name="permission">Permiso</string>
     <string name="permissions_media_manager">Administrador Multimedia</string>
     <string name="permissions_media_manager_desc">Mejor y mas rápida papelera/eliminar/copiar/mover</string>
     <string name="permissions_media_management">Administración de Multimedia</string>

app/src/main/res/values-fr/strings.xml
@@ -101,7 +101,7 @@
     <string name="trash_no_auto_delete">Les médias dans la corbeille ne seront pas automatiquement supprimés</string>
     <string name="trash_empty_desc">Cela supprime tous les médias dans la corbeille, cette action ne peut être annulée</string>
     <string name="trash_empty_confirm">Vider la corbeille</string>
-    <string name="permissions">Permission</string>
+    <string name="permission">Permission</string>
     <string name="permissions_media_manager">Gestionnaire de médias</string>
     <string name="permissions_media_manager_desc">Actions de suppression/corbeille/copie/déplacement plus rapides et efficaces</string>
     <string name="permissions_overwrite_date_on_move">Changer la date du média déplacé</string>

app/src/main/res/values-in/strings.xml
@@ -43,7 +43,7 @@
     <string name="editing_undo_last">Kembalikan tindakan terakhir</string>
     <string name="editing_scale_paint">Ganti ukuran kuas lukisan atau teks</string>
     <string name="editing_scale_zoom">Perbesar daripada menskalakan ukuran font</string>
-    <string name="permissions">Izin</string>
+    <string name="permission">Izin</string>
     <string name="permissions_media_manager">Pengelolaan Media</string>
     <string name="permissions_media_manager_desc">Sampah/hapus/salin/pindah yang lebih baik dan cepat</string>
     <string name="permissions_media_management">Pengelolaan Media</string>

app/src/main/res/values-pl/strings.xml
@@ -193,7 +193,7 @@
     <string name="trash_empty">Opróżnić kosz?</string>
     <string name="trash_empty_desc">Spowoduje to usunięcie wszystkich elementów z kosza. Tej czynności nie można cofnąć</string>
     <string name="trash_empty_confirm">Opróżnij</string>
-    <string name="permissions">Upoważnienie</string>
+    <string name="permission">Upoważnienie</string>
     <string name="permissions_media_manager">Menedżer multimediów</string>
     <string name="permissions_media_manager_desc">Lepsze i szybsze usuwanie/kopiowanie/przenoszenie</string>
     <string name="permissions_media_management">Zarządzanie multimediami</string>

commit bf18b69a102c3221c8eecfec4995da8edfa6b273

fix: crash when installing builds after commit e0f9b4495bed4106a8fbade81a9d357e8770eced
caused by incorrect resource lookup by index instead of id

Signed-off-by: kaii_lb <imkaiilb@gmail.com>

app/src/main/java/com/kaii/photos/compose/app_bars/MainBars.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
@@ -188,7 +189,7 @@ fun MainAppBottomBar(
     val state = rememberLazyListState(
         initialFirstVisibleItemIndex =
             tabs.indexOf(
-                if (mainTab == DefaultTabs.TabTypes.secure) DefaultTabs.TabTypes.photos else mainTab
+                if (mainTab == DefaultTabs.TabTypes.secure || mainTab !in tabs) tabs.first() else mainTab
             )
     )
 
@@ -278,13 +279,16 @@ fun MainAppBottomBar(
                                 })
                         ) {
                             items(
-                                count = tabs.size
-                            ) { index ->
-                                val tab = tabs[index].let {
+                                items = tabs
+                            ) { item ->
+                                val tab = item.let {
                                     it.copy(
                                         name =
-                                            if (it in DefaultTabs.extendedList) resources.getString(it.id)
-                                            else it.name
+                                            DefaultTabs.extendedList.find { tab ->
+                                                tab == item
+                                            }?.let { tab ->
+                                                resources.getString(tab.id)
+                                            } ?: it.name
                                     )
                                 }
 

app/src/main/java/com/kaii/photos/datastore/PreferencesClasses.kt
@@ -670,10 +670,8 @@ class SettingsDefaultTabsImpl(
         try {
             val typedList = separatedList
                 .toMutableList()
-                .apply {
-                    removeAll { item ->
-                        item == ""
-                    }
+                .filter { item ->
+                    item != ""
                 }
                 .map { serialized ->
                     json.decodeFromString<BottomBarTab>(serialized)
@@ -712,6 +710,12 @@ class SettingsDefaultTabsImpl(
 
             list.forEach { tab ->
                 stringList += json.encodeToString(tab) + separator
+            }
+
+            val default = json.decodeFromString<BottomBarTab>(it[defaultTab] ?: json.encodeToString(DefaultTabs.TabTypes.photos))
+
+            if (default !in list) {
+                it[defaultTab] = json.encodeToString(list.first())
             }
 
             it[tabList] = stringList

app/src/main/java/com/kaii/photos/datastore/PreferencesOptions.kt
@@ -74,84 +74,67 @@ object DefaultTabs {
 @Serializable
 enum class StoredDrawable(
     @param:DrawableRes val filled: Int,
-    @param:DrawableRes val nonFilled: Int,
-    val storedId: Int
+    @param:DrawableRes val nonFilled: Int
 ) {
     PhotoGrid(
         filled = R.drawable.photogrid_filled,
-        nonFilled = R.drawable.photogrid,
-        storedId = 0
+        nonFilled = R.drawable.photogrid
     ),
 
     SecureFolder(
         filled = R.drawable.secure_folder_filled,
-        nonFilled = R.drawable.secure_folder,
-        storedId = 2
+        nonFilled = R.drawable.secure_folder
     ),
 
     Albums(
         filled = R.drawable.albums_filled,
-        nonFilled = R.drawable.albums,
-        storedId = 4
+        nonFilled = R.drawable.albums
     ),
 
     Search(
         filled = R.drawable.search,
-        nonFilled = R.drawable.search,
-        storedId = 6
+        nonFilled = R.drawable.search
     ),
 
     Favourite(
         filled = R.drawable.favourite_filled,
-        nonFilled = R.drawable.favourite,
-        storedId = 7
+        nonFilled = R.drawable.favourite
     ),
 
     Star(
         filled = R.drawable.star_filled,
-        nonFilled = R.drawable.star,
-        storedId = 8
+        nonFilled = R.drawable.star
     ),
 
     Bolt(
         filled = R.drawable.bolt_filled,
-        nonFilled = R.drawable.bolt,
-        storedId = 9
+        nonFilled = R.drawable.bolt
     ),
 
     Face(
         filled = R.drawable.face_filled,
-        nonFilled = R.drawable.face,
-        storedId = 10
+        nonFilled = R.drawable.face
     ),
 
     Pets(
         filled = R.drawable.pets,
-        nonFilled = R.drawable.pets,
-        storedId = 11
+        nonFilled = R.drawable.pets
     ),
 
     Motorcycle(
         filled = R.drawable.motorcycle_filled,
-        nonFilled = R.drawable.motorcycle,
-        storedId = 12
+        nonFilled = R.drawable.motorcycle
     ),
 
     Motorsports(
         filled = R.drawable.motorsports_filled,
-        nonFilled = R.drawable.motorsports,
-        storedId = 13
+        nonFilled = R.drawable.motorsports
     ),
 
     Trash(
         filled = R.drawable.trash,
-        nonFilled = R.drawable.trash_filled,
-        storedId = 14
-    );
-
-    companion object {
-        fun toResId(storedId: Int) = entries.first { it.storedId == storedId }
-    }
+        nonFilled = R.drawable.trash_filled
+    )
 }
 
 @Serializable
@@ -170,7 +153,7 @@ data class BottomBarTab(
                     listOf(
                         it.name,
                         it.albumPaths,
-                        it.icon.storedId,
+                        StoredDrawable.entries.indexOf(it.icon),
                         it.id,
                         it.isCustom
                     )
@@ -179,7 +162,7 @@ data class BottomBarTab(
                     BottomBarTab(
                         name = it[0] as String,
                         albumPaths = it[1] as List<String>,
-                        icon = StoredDrawable.toResId(storedId = it[2] as Int),
+                        icon = StoredDrawable.entries[it[2] as Int],
                         id = it[3] as Int,
                         isCustom = it[4] as Boolean
                     )
@@ -193,10 +176,11 @@ data class BottomBarTab(
 
         other as BottomBarTab
 
-        // ignore index since it changes often and doesn't affect the tab itself
+        // ignore id since it changes often and doesn't affect the tab itself
         if (name != other.name) return false
         if (albumPaths != other.albumPaths) return false
         if (icon != other.icon) return false
+        if (isCustom != other.isCustom) return false
 
         return true
     }