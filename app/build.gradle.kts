5e105075dacd8430844aa7f699e304aa49fc8aae
kaii_lb <imkaiilb@gmail.com>
2025-12-23 22:45:04 +0200

    build(version): update changelog & version bump
    Signed-off-by: kaii_lb <imkaiilb@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.kaii.photos"
         minSdk = 30
         targetSdk = 36
-        versionCode = 120
-        versionName = "v1.2.0"
+        versionCode = 130
+        versionName = "v1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
