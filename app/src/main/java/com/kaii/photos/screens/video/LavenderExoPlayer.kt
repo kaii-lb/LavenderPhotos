kaii-lb/LavenderPhotos

commit b784d720ec315ab76ff65c73732ea6fdd705386f

fix: reusing an exoplayer after its released fix: cropping issues with video exporting

app/src/main/java/com/kaii/photos/screens/video/LavenderExoPlayer.kt
@@ -37,7 +37,8 @@ class LavenderExoPlayer(
     onDurationChanged: (duration: Float) -> Unit,
     onCurrentPositionChanged: (position: Float) -> Unit,
     onPlaybackStateChanged: (state: Int) -> Unit,
-    onAudioTracksChanged: (audioTrack: List<AudioTrack>) -> Unit
+    onAudioTracksChanged: (audioTrack: List<AudioTrack>) -> Unit,
+    onPlayingChanged: (playing: Boolean) -> Unit
 ) {
     data class AudioTrack(
         val language: String,
@@ -142,7 +143,13 @@ class LavenderExoPlayer(
             }
 
             override fun onTracksChanged(tracks: Tracks) {
+                super.onTracksChanged(tracks)
                 onAudioTracksChanged(getAudioTracks(tracks))
+            }
+
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                super.onIsPlayingChanged(isPlaying)
+                onPlayingChanged(isPlaying)
             }
         }
 

app/src/main/java/com/kaii/photos/screens/video/VideoPlayerState.kt
@@ -63,6 +63,7 @@ class VideoPlayerState(
     private var hideTimeout = 0L
     private var loopMode = 0
     private var muteOnStart = true
+    private var isReleased = false
 
     /** In Seconds */
     var currentPosition by mutableFloatStateOf(0f)
@@ -114,6 +115,10 @@ class VideoPlayerState(
 
             val current = player.getAudioTrack() ?: tracks.firstOrNull()?.language
             current?.let { setAudioTrack(it) }
+        },
+        onPlayingChanged = { playing ->
+            if (playing) play()
+            else pause()
         }
     )
 
@@ -169,20 +174,28 @@ class VideoPlayerState(
     }
 
     fun toggleMute() {
+        if (isReleased) return
+
         isMuted = !isMuted
         player.setMute(isMuted)
     }
 
     fun resetMute() = coroutineScope.launch {
+        if (isReleased) return@launch
+
         isMuted = muteOnStart && !isOpenWithView
     }
 
     fun toggleRepeatMode() {
+        if (isReleased) return
+
         isRepeatModeOn = !isRepeatModeOn
         player.setRepeatMode(isRepeatModeOn)
     }
 
     fun release(context: Context) {
+        isReleased = true
+
         player.release()
 
         context.appModule.scope.launch(Dispatchers.IO) {
@@ -202,6 +215,8 @@ class VideoPlayerState(
         shouldPlay: () -> Boolean,
         decryptProgress: (progress: Float) -> Unit = {}
     ) {
+        if (isReleased) return
+
         this.loop = loop
         this.shouldPlay = shouldPlay
         this.audioTracks.clear()
@@ -284,7 +299,7 @@ class VideoPlayerState(
     }
 
     fun play() {
-        if (!shouldPlay()) return
+        if (!shouldPlay() || isReleased) return
 
         isPlaying = true
 
@@ -319,6 +334,8 @@ class VideoPlayerState(
     }
 
     fun pause() {
+        if (isReleased) return
+
         isPlaying = false
         controlsVisible = true
         playingJob?.cancel()
@@ -331,12 +348,16 @@ class VideoPlayerState(
     }
 
     fun seekBack() {
+        if (isReleased) return
+
         val prev = isPlaying
         player.seekBack()
         isPlaying = prev
     }
 
     fun seekForward() {
+        if (isReleased) return
+
         val prev = isPlaying
         player.seekForward()
         isPlaying = prev
@@ -344,6 +365,8 @@ class VideoPlayerState(
 
     /** @param position in millisecond */
     fun seekTo(position: Long) {
+        if (isReleased) return
+
         val prev = isPlaying
         player.seekTo(position)
         currentPosition = player.currentPosition
@@ -351,27 +374,45 @@ class VideoPlayerState(
     }
 
     fun setPlaybackSpeed(speed: Float) {
+        if (isReleased) return
+
         if (speed !in 0.5f..4f) return
 
         player.setPlaybackSpeed(speed)
     }
 
-    fun setVolume(volume: Float) = player.setVolume(volume)
+    fun setVolume(volume: Float) {
+        if (isReleased) return
+
+        player.setVolume(volume)
+    }
 
     @androidx.annotation.OptIn(UnstableApi::class)
     fun applyEffects(effectList: List<Effect>) {
+        if (isReleased) return
+
         player.applyEffects(effectList)
     }
 
-    fun linkPlayerView(playerView: PlayerView) = player.linkPlayerView(playerView)
+    fun linkPlayerView(playerView: PlayerView) {
+        if (isReleased) return
+
+        player.linkPlayerView(playerView)
+    }
 
     fun setAudioTrack(language: String) {
+        if (isReleased) return
+
         if (player.setAudioTrack(language)) {
             selectedAudioTrack = audioTracks.find { it.language == language }
         }
     }
 
-    fun getFrameRate() = player.getFrameRate()
+    fun getFrameRate(): Float? {
+        if (isReleased) return null
+
+        return player.getFrameRate()
+    }
 }
 
 @Composable

app/src/main/java/com/kaii/photos/file_management/editing/GenericFileEditor.kt
@@ -338,6 +338,7 @@ interface GenericFileEditor {
                             )
                             .build()
                     )
+                    .setEnableFallback(true)
                     .build()
             )
             .apply {
@@ -415,8 +416,15 @@ interface GenericFileEditor {
             val newWidth = (basicVideoData.width * widthPercent).toInt()
             val newHeight = (basicVideoData.height * heightPercent).toInt()
 
+            val safeWidth = newWidth - (newWidth % 2)
+            val safeHeight = newHeight - (newHeight % 2)
+
             modList.add(
-                Presentation.createForShortSide(if (newWidth > newHeight) newWidth else newHeight)
+                Presentation.createForWidthAndHeight(
+                    safeWidth,
+                    safeHeight,
+                    Presentation.LAYOUT_SCALE_TO_FIT
+                )
             )
         }
 

commit 89940fbee558d258cdf888127998e43525586603

revert: part of b784d720ec315ab76ff65c73732ea6fdd705386f
introduced far too many bugs to keep

app/src/main/java/com/kaii/photos/screens/video/LavenderExoPlayer.kt
@@ -38,8 +38,7 @@ class LavenderExoPlayer(
     onDurationChanged: (duration: Float) -> Unit,
     onCurrentPositionChanged: (position: Float) -> Unit,
     onPlaybackStateChanged: (state: Int) -> Unit,
-    onAudioTracksChanged: (audioTrack: List<AudioTrack>) -> Unit,
-    onPlayingChanged: (playing: Boolean) -> Unit
+    onAudioTracksChanged: (audioTrack: List<AudioTrack>) -> Unit
 ) {
     companion object {
         private val TAG = LavenderExoPlayer::class.qualifiedName
@@ -150,11 +149,6 @@ class LavenderExoPlayer(
             override fun onTracksChanged(tracks: Tracks) {
                 super.onTracksChanged(tracks)
                 onAudioTracksChanged(getAudioTracks(tracks))
-            }
-
-            override fun onIsPlayingChanged(isPlaying: Boolean) {
-                super.onIsPlayingChanged(isPlaying)
-                onPlayingChanged(isPlaying)
             }
         }
 

app/src/main/java/com/kaii/photos/screens/video/VideoPlayerState.kt
@@ -118,10 +118,6 @@ class VideoPlayerState(
 
             val current = player.getAudioTrack() ?: tracks.firstOrNull()?.language
             current?.let { setAudioTrack(it) }
-        },
-        onPlayingChanged = { playing ->
-            if (playing) play()
-            else pause()
         }
     )
 