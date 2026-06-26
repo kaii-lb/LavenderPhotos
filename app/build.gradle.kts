d6e3c9b30b2d1e4c7f8b66c757504ac54bb4f3dd
kaii_lb <imkaiilb@gmail.com>
2026-06-26 19:46:54 +0300

    build: bump version & update changelog build(deps): update to lavender-immich-integration v2.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.kaii.photos"
         minSdk = 30
         targetSdk = 37
-        versionCode = 201
-        versionName = "v2.0.1"
+        versionCode = 202
+        versionName = "v2.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
