9666360d7684c1845584f71a95b83e8d0ebf745e
kaii_lb <imkaiilb@gmail.com>
2026-08-29 11:29:23 +0300

    fix: crash when XML parsing fails for live photos
    Signed-off-by: kaii_lb <imkaiilb@gmail.com>

--- app/src/main/java/com/kaii/photos/helpers/motion_photo/MotionPhotoState.kt
+++ app/src/main/java/com/kaii/photos/helpers/motion_photo/MotionPhotoState.kt
@@ -15,6 +15,7 @@ import androidx.exifinterface.media.ExifInterface
 import androidx.media3.common.util.UnstableApi
 import com.kaii.photos.mediastore.MediaType
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.serializer
 import nl.adaptivity.xmlutil.ExperimentalXmlUtilApi
@@ -22,7 +23,7 @@ import nl.adaptivity.xmlutil.serialization.DefaultXmlSerializationPolicy
 import nl.adaptivity.xmlutil.serialization.UnknownChildHandler
 import nl.adaptivity.xmlutil.serialization.XML
 import nl.adaptivity.xmlutil.serialization.XmlParsingException
-import java.io.FileNotFoundException
+import kotlin.time.Duration.Companion.milliseconds
 
 private const val TAG = "com.kaii.photos.helpers.MotionPhoto"
 
@@ -38,6 +39,12 @@ class MotionPhotoState(
         isMotionPhoto = false
     }
 
+    private sealed interface ParsingState {
+        data class Parsed(val xmpMeta: XmpMeta) : ParsingState
+        object HasNone : ParsingState
+        object Failed : ParsingState
+    }
+
     suspend fun getFor(
         uri: String,
         type: MediaType
@@ -47,10 +54,17 @@ class MotionPhotoState(
             return@withContext
         }
 
-        val xmp = getXmpData(uri.toUri())
+        var trialCount = 0
+        var parsedData = parseXmpFromUri(uri.toUri())
 
-        isMotionPhoto = if (xmp != null) {
-            xmp.rdf.description.motionPhoto == 1
+        while (parsedData is ParsingState.Failed && trialCount < 5) {
+            trialCount += 1
+            parsedData = parseXmpFromUri(uri.toUri())
+            delay(500.milliseconds)
+        }
+
+        isMotionPhoto = if (parsedData is ParsingState.Parsed) {
+            parsedData.xmpMeta.rdf.description.motionPhoto == 1
         } else {
             false
         }
@@ -58,44 +72,51 @@ class MotionPhotoState(
 
     @Suppress("DEPRECATION")
     @kotlin.OptIn(ExperimentalXmlUtilApi::class)
-    private fun getXmpData(uri: Uri): XmpMeta? {
+    private fun parseXmpFromUri(uri: Uri): ParsingState =
         try {
-            val inputStream = context.contentResolver.openInputStream(uri) ?: return null
+            val inputStream = context.contentResolver.openInputStream(uri) ?: return ParsingState.Failed
             val exifInterface = ExifInterface(inputStream)
 
             val xmpData = exifInterface.getAttribute(ExifInterface.TAG_XMP)
 
-            if (xmpData != null) {
-                val serializer = serializer<XmpMeta>()
-                val xml = XML {
-                    fast()
-                    autoPolymorphic = true
-
-                    // ignore unknown keys
-                    val policy = DefaultXmlSerializationPolicy.BuilderCompat()
-                        .apply {
-                            unknownChildHandler = UnknownChildHandler { _, _, _, _, _ ->
-                                emptyList()
-                            }
-                        }
-                        .build()
-
-                    this.policy = policy
-                }
-
+            if (xmpData == null) {
                 inputStream.close()
+                return ParsingState.HasNone
+            }
 
-                return xml.decodeFromString(serializer, xmpData)
+            val serializer = serializer<XmpMeta>()
+            val xml = XML {
+                fast()
+                autoPolymorphic = true
+
+                // ignore unknown keys
+                val policy = DefaultXmlSerializationPolicy.BuilderCompat()
+                    .apply {
+                        unknownChildHandler = UnknownChildHandler { _, _, _, _, _ ->
+                            emptyList()
+                        }
+                    }
+                    .build()
+
+                this.policy = policy
             }
 
             inputStream.close()
-        } catch (_: XmlParsingException) {
-        } catch (e: FileNotFoundException) {
-            Log.d(TAG, e.message.toString())
-        }
 
-        return null
-    }
+            return ParsingState.Parsed(
+                xmpMeta = xml.decodeFromString(serializer, xmpData)
+            )
+        } catch (e: XmlParsingException) {
+            Log.d(TAG, "Xml parsing failed. ${e.message}")
+
+            ParsingState.Failed
+        } catch (e: IllegalStateException) {
+            Log.d(TAG, "Cannot access content $uri. ${e.message}")
+            ParsingState.Failed
+        } catch (e: Throwable) {
+            Log.d(TAG, e.message.toString())
+            ParsingState.HasNone
+        }
 }
 
 @Composable
